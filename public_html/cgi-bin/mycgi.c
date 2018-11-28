cd72a0fefbcd24fd02f79de5d76559f4c86ea5c8
lukebrossman <lukebrossman1@gmail.com>
2018-11-27 19:55:36 -0800

    public_html so I can modify on linux

---
+++ public_html/cgi-bin/mycgi.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#define MAX 10000
+typedef struct {
+    char *name;
+    char *value;
+} ENTRY;
+
+ENTRY entry[MAX];
+
+main(int argc, char *argv[]) 
+{
+  int i, m, r;
+  char cwd[128];
+
+  m = getinputs();    // get user inputs name=value into entry[ ]
+  getcwd(cwd, 128);   // get CWD pathname
+
+  printf("Content-type: text/html\n\n");
+  printf("<p>pid=%d uid=%d cwd=%s\n", getpid(), getuid(), cwd);
+
+  printf("<H1>Echo Your Inputs</H1>");
+  printf("You submitted the following name/value pairs:<p>");
+ 
+  for(i=0; i <= m; i++)
+     printf("%s = %s<p>", entry[i].name, entry[i].value);
+  printf("<p>");
+
+  if (entry[0].value == "mkdir")
+  {
+    printf("mkdir recieved");
+    mkdir(entry[1].value, 0777);
+  }
+  if (entry[0].value == "rmdir")
+  {
+    rmdir(entry[1].value);
+    printf("rmdir recieved");
+  }
+
+  /*****************************************************************
+   Write YOUR C code here to processs the command
+         mkdir dirname
+         rmdir dirname
+         rm    filename
+         cat   filename
+         cp    file1 file2
+         ls    [dirname] <== ls CWD if no dirname
+  *****************************************************************/
+ 
+  // create a FORM webpage for user to submit again 
+  printf("</title>");
+  printf("</head>");
+  printf("<body bgcolor=\"#FF0000\" link=\"#330033\" leftmargin=8 topmargin=8");
+  printf("<p>------------------ DO IT AGAIN ----------------\n");
+  
+  printf("<FORM METHOD=\"POST\" ACTION=\"http://cs360.eecs.wsu.edu/~brossman/cgi-bin/mycgi.bin\">");
+  
+  printf("Enter command : <INPUT NAME=\"command\"> <P>");
+  printf("Enter filename1: <INPUT NAME=\"filename1\"> <P>");
+  printf("Enter filename2: <INPUT NAME=\"filename2\"> <P>");
+  printf("Submit command: <INPUT TYPE=\"submit\" VALUE=\"Click to Submit\"><P>");
+  printf("</form>");
+  printf("------------------------------------------------<p>");
+
+  printf("</body>");
+  printf("</html>");
+}
